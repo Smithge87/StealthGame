99621e73b51b540d1591a2a269eb0223027dcb4e
Grant Smith <gsmith@tampabay.com>
2020-05-01 11:26:55 -0400

    guard is moving around waypoints

--- Assets/Scripts/Guard.cs
+++ Assets/Scripts/Guard.cs
@@ -1,14 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class Guard : MonoBehaviour
 {
     public Transform pathHolder;
-
+    public float speed = 5f;
+    public float waitTime = .3f;
     void Start()
     {
-        
+        Vector3[] waypoints = new Vector3[pathHolder.childCount];
+        for (int i=0; i < waypoints.Length; i++)
+        {
+            waypoints[i] = pathHolder.GetChild(i).position;
+        }
+        StartCoroutine(FollowPath(waypoints));
+
     }
 
     void Update()
@@ -16,6 +24,27 @@ public class Guard : MonoBehaviour
         
     }
 
+    IEnumerator FollowPath (Vector3[] waypoints)
+    {
+        //-- there's time compexity here and i need to spend some time understanding it... 
+        transform.position = waypoints[0];
+        int targetWaypointIndex = 1;
+        Vector3 targetWaypoint = waypoints[targetWaypointIndex];
+        while (true)
+        {
+            //-- next line is pretty standard. loop below is a brain bender.
+            transform.position = Vector3.MoveTowards(transform.position, targetWaypoint, speed * Time.deltaTime);
+            if(transform.position == targetWaypoint)
+            {
+                //-- modulo below makes the loop start over when it hits zero which is kinda mind blowing
+                targetWaypointIndex = (targetWaypointIndex + 1) % waypoints.Length;
+                targetWaypoint = waypoints[targetWaypointIndex];
+                yield return new WaitForSeconds(waitTime);
+            }
+            //-- advance one per frame
+            yield return null;
+        }
+    }
 
     //-- Handles drawing the waypoint Gizmos
     private void OnDrawGizmos()
